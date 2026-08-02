748b52e0b79f4b2199fc433495c5883ecd4b3f3d
uPiscium <upiscium@gmail.com>
2026-08-02 11:21:51 +0900

    feat: add shared portfolio layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
-  site: 'https://blog.upiscium.dev',
+  site: 'https://upiscium.dev',
   output: 'static',
   markdown: {
     remarkPlugins: [remarkGfm],
